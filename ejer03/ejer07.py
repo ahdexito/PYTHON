fbf0b05f37e32c424b5294080c84dce2dba66b04
Ahdexito <ags120120@gmail.com>
2025-10-07 13:41:57 +0200

    save

---
+++ ejer03/ejer07.py
@@ -0,0 +1,13 @@
+def mi_funcion_ejemplo(a, b, c):
+    resultado = a + b+ c
+    return resultado
+
+def programa_principal():
+    x = 10
+    y = 20
+    z = 30
+
+    print("la suma es: ", mi_funcion_ejemplo(x, y, z))
+
+if __name__ == "__main__":
+    programa_principal()
