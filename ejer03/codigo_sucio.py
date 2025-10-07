02dcf98496370bd0950deb24311a5e5954a5800c
Ahdexito <ags120120@gmail.com>
2025-10-07 13:25:53 +0200

    save

---
+++ ejer03/codigo_sucio.py
@@ -0,0 +1,9 @@
+def mi_funcion_ejemplo(a, b, c):
+    resultado = a + b + c
+    return resultado
+
+
+x = 10
+y = 20
+z = 30
+print("la suma es: ", mi_funcion_ejemplo(x, y, z))
--- ejer03/ejer06.py
+++ ejer03/ejer06.py
@@ -1,4 +1,14 @@
-nombre = input("Introduce tu nombre: ")
-producto = input("Introduce producto: ")
-precio = input("Introduce su precio: ")
+def imprimir_producto():
+    
+    nombre = input("Introduce tu nombre: ")
+    producto = input("Introduce producto: ")
+    precio = input("Introduce su precio: ")
 
+    print(f"{nombre}, gracias por tu compra. El producto {producto} tiene un precio de {precio}€")
+
+    print("{}, gracias por tu compra. El producto {} tiene un precio de {}€".format(nombre, producto, precio))
+
+    print(nombre,", gracias por tu compra. El producto",producto,"tiene un precio de",precio,"€",sep="-")
+
+if __name__ == "__main__":
+    imprimir_producto()
