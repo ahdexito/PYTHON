9a77b88b7c71eb7d98d71093ed057a3b6c1c836a
Ahdexito <ags120120@gmail.com>
2025-10-30 09:47:40 +0100

    save

--- tema05/ejer02/ejer02.py
+++ tema05/ejer02/ejer02.py
@@ -22,7 +22,7 @@ class ReservaHotel:
     
     # método setter
     def set_precio(self, nuevo_precio):
-        if (nuevo_precio > self._precio_total):
+        if nuevo_precio > self._precio_total:
             self._precio_total = nuevo_precio
         
     # método de instancia
@@ -41,7 +41,7 @@ class ReservaHotel:
         
     # método que reduce el precio si coincide el código
     def aplicar_descuento(self, porcentaje, codigo):
-        if (codigo == self.__codigo_confirmacion):
+        if codigo == self.__codigo_confirmacion:
             self._precio_total -= self._precio_total * porcentaje
     
 
